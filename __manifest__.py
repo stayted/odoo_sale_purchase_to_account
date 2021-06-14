422bfaf61284edd81f00beb22ccf1e02227ea86c
Silvio Benvegnù <silvio.benve@gmail.com>
2021-06-14 09:49:14 +0200

    manifest description

--- __manifest__.py
+++ __manifest__.py
@@ -1,16 +1,20 @@
 # -*- encoding: utf-8 -*-
 {
-    'name' : 'DA - incoterms from sale_order',
-    'version': '0.1',
+    'name' : 'DA - odoo_sale_purchase_to_account',
+    'version': '0.0.1',
     'summary': 'Module customization',
     'category': 'Custom Development',
-    'author': 'Silvio Benvegnù',
+    'author': 'Silvio Benvegnù @ Digital Automations',
     'description':
         """
 Digital Automations
 ====================
 
-Incoterms
+Copy fields from sale.order and purchase.order to account.move
+
+This module copies:
+
+- x_studio_incoterms from sale.order
 
         """,
     'data': [
